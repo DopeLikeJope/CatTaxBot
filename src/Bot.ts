d616f9d15addc7fe7c2b51fe42f6d80cef9d0f08
Joseph Perez <joseph_perez3@baylor.edu>
2022-05-27 15:32:54 -0500

    Setting up Docker containers

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,13 +1,19 @@
-import { Client } from "discord.js";
+import { Client, Intents } from "discord.js";
+import "dotenv/config";
 
 console.log("Bot is starting...");
 
 const client = new Client({
-  intents: []
+  intents: [
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+  ]
 });
+
+client.login(process.env.BOT_TOKEN);
 
 client.user?.setActivity(
   `Taxing ${client.guilds.cache.size} guilds for cat pcitures`
 );
-
-console.log(client);
